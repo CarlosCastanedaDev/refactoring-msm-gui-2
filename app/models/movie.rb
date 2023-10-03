d379a2cb40460dc62c302c57558a49c4fa7d2653
CarlosCastanedaDev <carlos@nemin.dev>
2023-10-03 11:10:06 -0500

    assignment complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -16,13 +16,23 @@ class Movie < ApplicationRecord
   validates(:director_id, presence: true)
   validates(:title, uniqueness: true)
 
-  def director
-    key = self.director_id
+  # def characters
+  #   key = self.id
+  #   the_many = Character.where( {:movie_id => key })
+  #   return the_many
+  # end 
 
-    matching_set = Director.where({ :id => key })
+  has_many :characters, class_name: "Character", foreign_key: "character_id"
 
-    the_one = matching_set.at(0)
+  belongs_to(:director, :class_name => "Director" , :foreign_key => "director_id")
 
-    return the_one
-  end
+  # def director
+  #   key = self.director_id
+
+  #   matching_set = Director.where({ :id => key })
+
+  #   the_one = matching_set.at(0)
+
+  #   return the_one
+  # end
 end
